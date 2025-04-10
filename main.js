b4c2f83261e6d1e15100827ae703321c45cd016e
gustavopes2023 <144125042+gustavopes2023@users.noreply.github.com>
2025-04-10 09:48:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,16 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-05-20T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-18T00:00:00");
+const tempoObjetivo3 = new Date("2025-11-30T00:00:00");
+const tempoObjetivo4 = new Date("2026-02-10T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
